836a02b63f920f0153e1901eb84faf4d14c638aa
lyagush0n0k <romas71ru@gmail.com>
2022-02-06 07:23:11 -0800

    Update time.bat

--- time.bat
+++ time.bat
@@ -1,7 +1,7 @@
 set /A numDays=10
 
 reg query HKEY_CURRENT_USER\time
-if %ERRORLEVEL% EQU 1 reg add HKEY_CURRENT_USER\time /v day /t REG_SZ
+if %ERRORLEVEL% EQU 1 reg add HKEY_CURRENT_USER\time /v day /t REG_SZ /d 100
 
 FOR /F "tokens=2* skip=2" %%a in ('reg query HKEY_CURRENT_USER\time /v day') do set /A dateReg=%%b
 
